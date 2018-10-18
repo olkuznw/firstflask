24d3a7adce7c01e973f843797e64bd397d83eeac
olkuznw <olkuznw@gmail.com>
2018-10-18 23:58:07 +0300

    simple form

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,10 +1,32 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 
 from models import Post, Tag
 
+from .forms import PostForm
+
+from app import db
+
 posts = Blueprint('posts', __name__, template_folder= 'templates')
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if 'POST' == request.method:
+        title = request.form.get('title', '')
+        body = request.form.get('body', '')
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('some error')
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
 
 @posts.route('/')
 def index():
@@ -12,7 +34,7 @@ def index():
     if search:
         posts = Post.query.filter(Post.title.contains(search) | Post.body.contains(search)).all()
     else:
-        posts = Post.query.all();
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('posts/index.html', posts=posts)
 
 @posts.route('/<slug>')
