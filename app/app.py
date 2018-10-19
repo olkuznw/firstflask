fbdac622997b19061433932755c4b82755a94205
olkuznw <olkuznw@gmail.com>
2018-10-20 00:06:15 +0300

    init admin panel

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 from config import Configuration
+
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
+
 from flask_script import Manager
+
+from flask_admin import Admin
+
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -15,3 +22,9 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+### Admin ###
+from models import *
+
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
